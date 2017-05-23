1ef89757f6794e304a1e63a756feb56330134fff
Allison Chan <allisonchanykei@gmail.com>
2017-05-22 22:25:57 -0400

    Allow companies to go to their personal page

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var company = getQueryVariable("company");
+    if (company) {
+        $("#CoverLetter").prop("href", "./doc/CoverLetter/" + company);
+        $("#CoverLetter").show();
+    } else {
+        $("#no-comp").show();
+        //prevent postback
+        if ($("#selectCompany").val()) {
+            $("#chooseCompany").show();
+        }
+        $("#selectCompany").on("change", function() {
+            if ($(this).val()) {
+                $("#chooseCompany").show();
+            } else {
+                $("#chooseCompany").hide();
+            }
+        })
+        $("#chooseCompany").on("click", function(e) {
+            window.location.href = window.location.href + "?company=" + $("#selectCompany").val();
+        })
+    }
+})
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1).toLowerCase();
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
