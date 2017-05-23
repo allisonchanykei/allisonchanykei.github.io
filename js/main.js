8341ef9922a8d6ce73b608d10f5c470af327fd1a
Allison Chan <allisonchanykei@gmail.com>
2017-05-22 23:08:28 -0400

    ensures company is part of the list

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var company = getQueryVariable("company");
-    if (company) {
+    var showLetter = 0 != $('#select-box option[value=' + company + ']').length;
+    if (showLetter) {
         $("#CoverLetter").prop("href", "./doc/CoverLetter/" + company + ".pdf");
         $("#CoverLetter").show();
     } else {
