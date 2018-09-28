92b5360f64b01636956697b2bb133a17dbc56cb1
Allison Chan <allisonchanykei@gmail.com>
2018-09-27 22:19:28 -0400

    made it simpler

--- js/main.js
+++ js/main.js
@@ -1,35 +1,7 @@
 $(document).ready(function() {
-    var company = getQueryVariable("company");
-    var showLetter = 0 != $('#selectCompany option[value=' + company + ']').length;
-    if (showLetter) {
+    $("#showMessage").on("click", function(e) {
         $("#comp").show();
-        //$("#CoverLetter").prop("href", "./doc/CoverLetter/" + company + ".pdf");
-        //$("#CoverLetter").show();
-    } else {
-        $("#no-comp").show();
-        //prevent postback
-        if ($("#selectCompany").val()) {
-            $("#chooseCompany").show();
-        }
-        $("#selectCompany").on("change", function() {
-            if ($(this).val()) {
-                $("#chooseCompany").show();
-            } else {
-                $("#chooseCompany").hide();
-            }
-        })
-        $("#chooseCompany").on("click", function(e) {
-            window.location.href = window.location.href + "?company=" + $("#selectCompany").val();
-        })
-    }
-})
-
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1).toLowerCase();
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable) { return pair[1]; }
-    }
-    return (false);
-}
+        $("#resume").show();
+        $("#showMessage").hide();
+    });
+});
